983bae62ba840605d1154ecab22b986bfe1c8a93
Ricardo <richoy2201@gmail.com>
2021-03-31 13:43:03 -0500

    Add sum logic, controller and unit test

---
+++ src/test/java/com/ricardo/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.ricardo.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
